f61fcdbe5ba8741314a7a720141cde440b14da7e
Minyi Yang <minyi.yang@hotmail.com>
2020-08-14 14:58:39 -0400

    first version of the static index page

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>SHOP</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="//db.onlinewebfonts.com/c/b6539b6d3432c623d8d4f9cc2a29589e?family=Berlin+Sans+FB+Demi" rel="stylesheet" type="text/css"/> 
+
+        <!-- Styles -->
+        <link href="css/main.css" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+        <div class="foot">
+            <p>Conditions of Use Private Notice Interest-Based Ads © 2008-2020, Faovershoes.com, Inc. or its affiliates</p>
+        </div>
+    </body>
+</html>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,89 +1,130 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')  
 
-        <title>SHOP</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Testing shop functionality
-                </div>
+@section('content')
+<div class="banner">
+    <div class="row">
+        <div class="col-4 m-tp-md">
+            <a href="#" traget="_blank" class="logo">FAVOUR SHOES</a>
+        </div>
+        <div class="col-4 m-tp-md">
+            <div class="search-bar">
+                <input type="text" class="search-input">
+                <input type="button" class="serach-btn">
             </div>
         </div>
-    </body>
-</html>
+        <div class="btn-groups col-4 m-tp-md">
+            <a href="#" traget="_blank"><img class="icon-login" src = "/images/login.png" ></a>
+            <a href="#" traget="_blank"><img class="icon-cart m-lft-sm " src = "/images/cart.png" ></a>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="flex-container">
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="/item" traget="_blank" class="imgbg"><img src = "/images/nike_shoe.png" ></a>
+            <a href="#" traget="_blank" class="link">
+                <p>Nike Sportswear</p>
+            </a>
+            <a href="/item" traget="_blank" class="link">
+                <p><strong>Air Max Traix 96</strong></p>
+                <p> 
+                    <span class="text-red">$98</span>
+                    <span class="text-line-through">($120)</span>
+                </p>
+            </a>
+        </div>
+    </div>
+</div>
+@endsection
+
